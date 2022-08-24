f79827f9d591778107796aed8f4722c41f092c7f
Gyeongman <rudakswhale@gmail.com>
2022-08-24 20:29:00 +0900

    Feat : 자동차가 몇대가 갈 것인지 추가

--- src/main/java/carraceing/Car.java
+++ src/main/java/carraceing/Car.java
@@ -1,19 +1,33 @@
 package carraceing;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Car {
-    private Random random = new Random();
-    private final int car;
 
-    public Car(int car) {
-        if (car == 0 ) {
-            throw new IllegalArgumentException();
+    private final Random random = new Random();
+    private final List<Integer> cars = new ArrayList<>();
+    private final int carCount;
+
+    public Car(int carCount) {
+        if (carCount == 0) {
+            throw new IllegalArgumentException("0이 입력되면 예외가 발생한다.");
         }
-        if (Character.isDigit(car)){
+        if (Character.isDigit(carCount)) {
             throw new IllegalArgumentException("문자열이 입력되면 예외가 발생한다.");
         }
-        this.car = car;
+        this.carCount = carCount;
     }
 
+    public void carDriving() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("자동차 대수는 몇 대 인가요");
+
+        int carCount = scanner.nextInt();
+        if (carCount == 0) {
+            throw new IllegalArgumentException("0이 입력되면 예외가 발생한다.");
+        }
+    }
 }
