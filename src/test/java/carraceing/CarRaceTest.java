6df256d93a66251775ca34e9f90f90dfb6dda0d4
Gyeongman <rudakswhale@gmail.com>
2022-08-24 23:09:07 +0900

    Test : 객체에 0값이 들어오면 예외발생

--- src/test/java/carraceing/CarRaceTest.java
+++ src/test/java/carraceing/CarRaceTest.java
@@ -1,20 +1,18 @@
 package carraceing;
 
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class CarRaceTest {
 
     @Test
     @DisplayName("0값이 들어오면 예외가 발생한다.")
     void throws_exception_zero_insert_car_count(){
-        Assertions.assertThatThrownBy(() -> new Car(0,0))
+        assertThatThrownBy(() -> new Car(0,0))
             .isInstanceOf(IllegalArgumentException.class);
     }
-
 
 }
