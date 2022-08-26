d604e4493111a81f4ebe01ea93d53b70e4cb71e2
Gyeongman <rudakswhale@gmail.com>
2022-08-26 20:28:34 +0900

    Test : 0값이 들어오면 예외가 발생한다.

--- src/test/java/carraceing/CarRaceTest.java
+++ src/test/java/carraceing/CarRaceTest.java
@@ -1,18 +1,18 @@
 package carraceing;
 
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarRaceTest {
 
-//    @Test
-//    @DisplayName("0값이 들어오면 예외가 발생한다.")
-//    void throws_exception_zero_insert_car_count(){
-//        assertThatThrownBy(() -> new Car(0,0))
-//            .isInstanceOf(IllegalArgumentException.class);
-//    }
-
+    @Test
+    @DisplayName("0값이 들어오면 예외가 발생한다")
+    void throws_exception_zero_insert() {
+        int input = 0;
+        Assertions.assertThatThrownBy(() -> CarRace.addCarCount(input))
+                  .isInstanceOf(IllegalArgumentException.class)
+                  .hasMessage("0값이 들어오면 예외가 발생한다");
+    }
 }
