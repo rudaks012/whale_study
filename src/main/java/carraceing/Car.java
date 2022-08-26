a8ba63222f900088552dba80231a91501f05ab10
Gyeongman <rudakswhale@gmail.com>
2022-08-26 19:37:50 +0900

    Feat : 전진시 포지션 값 추가

--- src/main/java/carraceing/Car.java
+++ src/main/java/carraceing/Car.java
@@ -1,5 +1,11 @@
 package carraceing;
 
 public class Car {
+    private int position;
+    public void forward(boolean forward) {
+        if(forward){
+            position++;
+        }
+    }
 
 }
