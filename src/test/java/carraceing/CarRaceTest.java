73a2cf6a5b2b86bfec2856d76d1637ebb8e9ab71
Gyeongman <rudakswhale@gmail.com>
2022-08-26 22:20:38 +0900

    Test : Car 객체 생성

--- src/test/java/carraceing/CarRaceTest.java
+++ src/test/java/carraceing/CarRaceTest.java
@@ -1,21 +1,25 @@
 package carraceing;
 
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class CarRaceTest {
+    private final Car car = new Car();
 
     @Test
     @DisplayName("0값이 들어오면 예외가 발생한다")
     void throws_exception_zero_insert() {
-        Car car = new Car();
         int input = 0;
 
-        Assertions.assertThatThrownBy(() -> car.addCarCount(input))
+        assertThatThrownBy(() -> car.addCarCount(input))
                   .isInstanceOf(IllegalArgumentException.class)
                   .hasMessage("0값이 들어오면 예외가 발생한다");
     }
